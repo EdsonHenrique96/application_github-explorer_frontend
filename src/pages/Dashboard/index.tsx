4fdf4ae2dffa3100d85db9ea67054104815faf23
Edson Batista <edson.batista@catho.com>
2020-08-23 15:08:11 -0300

    chore(code style): configs code style tools

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,49 +3,58 @@ import { FiChevronRight } from 'react-icons/fi';
 
 import { Title, Form, Repositories } from './styles';
 
-import githubExplorerLogo from '../../assets/github-explorer-logo.svg'
+import githubExplorerLogo from '../../assets/github-explorer-logo.svg';
 
 const Dashboard: React.FC = () => (
   <>
-    <img src={githubExplorerLogo} alt='Github Explorer'/>
+    <img src={githubExplorerLogo} alt="Github Explorer" />
     <Title>Explore repositórios no Github</Title>
     <Form>
-      <input type='text' placeholder='Nome do repositório'/>
-      <button>Pesquisar</button>
+      <input type="text" placeholder="Nome do repositório" />
+      <button type="submit">Pesquisar</button>
     </Form>
 
     <Repositories>
       <a href="test">
-        <img src='https://avatars2.githubusercontent.com/u/30643630?s=460&u=d87bede43db962a5aed9d352e0a18c53ccd57c73&v=4' alt='Edson Henrique'/>
+        <img
+          src="https://avatars2.githubusercontent.com/u/30643630?s=460&u=d87bede43db962a5aed9d352e0a18c53ccd57c73&v=4"
+          alt="Edson Henrique"
+        />
         <div>
           <strong>application_github-explorer</strong>
           <p>Um frontend ai</p>
         </div>
 
-        <FiChevronRight size={20}/>
+        <FiChevronRight size={20} />
       </a>
 
       <a href="test">
-        <img src='https://avatars2.githubusercontent.com/u/30643630?s=460&u=d87bede43db962a5aed9d352e0a18c53ccd57c73&v=4' alt='Edson Henrique'/>
+        <img
+          src="https://avatars2.githubusercontent.com/u/30643630?s=460&u=d87bede43db962a5aed9d352e0a18c53ccd57c73&v=4"
+          alt="Edson Henrique"
+        />
         <div>
           <strong>application_github-explorer</strong>
           <p>Um frontend ai</p>
         </div>
 
-        <FiChevronRight size={20}/>
+        <FiChevronRight size={20} />
       </a>
-      
+
       <a href="test">
-        <img src='https://avatars2.githubusercontent.com/u/30643630?s=460&u=d87bede43db962a5aed9d352e0a18c53ccd57c73&v=4' alt='Edson Henrique'/>
+        <img
+          src="https://avatars2.githubusercontent.com/u/30643630?s=460&u=d87bede43db962a5aed9d352e0a18c53ccd57c73&v=4"
+          alt="Edson Henrique"
+        />
         <div>
           <strong>application_github-explorer</strong>
           <p>Um frontend ai</p>
         </div>
 
-        <FiChevronRight size={20}/>
+        <FiChevronRight size={20} />
       </a>
     </Repositories>
   </>
-)
+);
 
 export default Dashboard;
